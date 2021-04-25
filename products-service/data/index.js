d8aab59d02dc4ecbedd8e2ec407a64535c32e8b1
Taofeek <taofeekolamilekan218@gmail.com>
2021-04-25 06:04:16 +0100

    URL update

--- products-service/data/index.js
+++ products-service/data/index.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
-var url1 = "mongodb+srv://taocoder:" + encodeURIComponent("@fY8fnLj!#n*9Y#") + "@cluster0.yjg9n.mongodb.net/ecommerce?retryWrites=true&w=majority";
+var url1 = "atlas URL"
 
 exports.getConnection = async () => {
 	const db = await MongoClient.connect(url1, { useUnifiedTopology: true });
